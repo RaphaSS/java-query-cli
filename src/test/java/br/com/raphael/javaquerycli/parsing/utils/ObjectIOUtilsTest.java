692f321fab842859f65ce9b5dc88182b20f44eb4
RaphaSS <raphass22@gmail.com>
2017-11-05 17:47:32 -0200

    Verificar execução dos testes via linha de comando

--- src/test/java/br/com/raphael/javaquerycli/parsing/utils/ObjectIOUtilsTest.java
+++ src/test/java/br/com/raphael/javaquerycli/parsing/utils/ObjectIOUtilsTest.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -20,9 +21,16 @@ import junitx.framework.ListAssert;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class ObjectIOUtilsTest {
 
+	private static File file;
+
+	@BeforeClass
+	public static void setUp() {
+		final String datasetFile = System.getProperty("datasetfile");
+		file = new File(datasetFile);
+	}
+
 	@Test
 	public void leArquivo() {
-		final File file = new File("data/test.csv");
 		try(InputStream inputStream = new FileInputStream(file)) {
 			final List<City> readCities = ObjectIOUtils.read(inputStream, City.class);
 
