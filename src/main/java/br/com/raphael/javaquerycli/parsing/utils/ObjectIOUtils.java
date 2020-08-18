a5dbf8638fd9c0590481ee8b13feb41ddbd6678b
Raphael <raphael.silveira@valemobi.com.br>
2020-08-18 10:04:41 -0300

    clean code

--- src/main/java/br/com/raphael/javaquerycli/parsing/utils/ObjectIOUtils.java
+++ src/main/java/br/com/raphael/javaquerycli/parsing/utils/ObjectIOUtils.java
@@ -1,6 +1,7 @@
 package br.com.raphael.javaquerycli.parsing.utils;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.LF;
 
 import java.io.InputStream;
@@ -10,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import br.com.raphael.javaquerycli.parsing.exception.InvalidDataSetException;
 import br.com.raphael.javaquerycli.parsing.exception.ParsingException;
@@ -18,14 +18,16 @@ import br.com.raphael.javaquerycli.parsing.exception.PropertyNotFoundException;
 
 public class ObjectIOUtils {
 
+	public static final String DELIMITER = ",";
+
 	public static <T> List<T> read(final InputStream inputStream, final Class<T> clazz) throws ParsingException {
 		List<T> list;
 		try(final Scanner scanner = new Scanner(inputStream);) {
-			final String[] headers = scanner.nextLine().split(",");
+			final String[] headers = scanner.nextLine().split(DELIMITER);
 
 			for(list = new ArrayList<>(); scanner.hasNextLine();) {
 				try {
-					final String[] values = scanner.nextLine().split(",");
+					final String[] values = scanner.nextLine().split(DELIMITER);
 
 					if(headers.length != values.length)
 						throw new InvalidDataSetException();
@@ -46,35 +48,44 @@ public class ObjectIOUtils {
 
 	public static <T> void write(final List<T> list, final OutputStream outputStream, final Class<T> clazz) {
 		final PrintWriter writer = new PrintWriter(outputStream);
-		final List<String> headers = Arrays.asList(clazz.getDeclaredFields()).stream()
-			.map(f -> f.getDeclaredAnnotation(br.com.raphael.javaquerycli.parsing.annotation.Field.class).value())
-			.collect(Collectors.toList());
+		final List<String> headers = getHeaderFields(clazz);
 		final String strHeaders = headers.stream()
-			.collect(joining(","));
+			.collect(joining(DELIMITER));
 
-		writer.print(strHeaders);
-		writer.print(LF);
+		writeContent(writer, strHeaders);
 
-		list.stream()
+		list
 			.forEach(e -> {
-				writer.print(
-					headers.stream()
-						.map(h -> {
-							Object value;
-							try {
-								value = ObjectUtils.getValue(e, h);
-								if(value instanceof Boolean)
-									return Boolean.TRUE.equals(value) ? value.toString() : "";
-								else return value.toString();
-							} catch(final PropertyNotFoundException e1) {
-								return null;
-							}
-						})
-						.collect(joining(",")));
-				writer.print(LF);
+				String line = headers.stream()
+					.map(h -> getValue(e, h))
+					.collect(joining(DELIMITER));
+				writeContent(writer, line);
 			});
 
 		writer.flush();
+	}
+
+	private static <T> List<String> getHeaderFields(Class<T> clazz) {
+		return Arrays.stream(clazz.getDeclaredFields())
+			.map(f -> f.getDeclaredAnnotation(br.com.raphael.javaquerycli.parsing.annotation.Field.class).value())
+			.collect(toList());
+	}
+
+	private static <T> String getValue(T object, String property) {
+		Object value;
+		try {
+			value = ObjectUtils.getValue(object, property);
+			if(value instanceof Boolean)
+				return Boolean.TRUE.equals(value) ? value.toString() : "";
+			return value != null ? value.toString() : null;
+		} catch(final PropertyNotFoundException e1) {
+			return null;
+		}
+	}
+
+	private static void writeContent(PrintWriter writer, String line) {
+		writer.print(line);
+		writer.print(LF);
 	}
 
 }
