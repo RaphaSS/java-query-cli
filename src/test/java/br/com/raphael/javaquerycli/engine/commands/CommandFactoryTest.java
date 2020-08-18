4d6fe42c01cebf3e892755a8c42979a9f2f9ba6f
Raphael <raphael.silveira@valemobi.com.br>
2020-08-18 10:28:38 -0300

    Adds commands edge cases unit tests

--- src/test/java/br/com/raphael/javaquerycli/engine/commands/CommandFactoryTest.java
+++ src/test/java/br/com/raphael/javaquerycli/engine/commands/CommandFactoryTest.java
@@ -1,12 +1,16 @@
 package br.com.raphael.javaquerycli.engine.commands;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.Test;
 
 import br.com.raphael.javaquerycli.engine.commands.impl.CountAllCommand;
 import br.com.raphael.javaquerycli.engine.commands.impl.CountDistinctCommand;
+import br.com.raphael.javaquerycli.engine.commands.impl.ExitCommand;
 import br.com.raphael.javaquerycli.engine.commands.impl.FilterCommand;
+import br.com.raphael.javaquerycli.engine.commands.impl.HelpCommand;
+import br.com.raphael.javaquerycli.engine.exception.CommandNotFoundException;
 import br.com.raphael.javaquerycli.engine.exception.InterpretationException;
 
 public class CommandFactoryTest {
@@ -44,6 +48,25 @@ public class CommandFactoryTest {
         FilterCommand filter = (FilterCommand) c;
         assertThat(filter.getProperty()).isEqualTo("mesoregion");
         assertThat(filter.getValue()).isEqualTo("Grande Florianópolis");
+    }
+
+    @Test
+    public void shouldCreate_helpCommand() throws InterpretationException {
+        Command c = CommandFactory.getInstance("help");
+        assertThat(c).isInstanceOf(HelpCommand.class);
+    }
+
+    @Test
+    public void shouldCreate_exitCommand() throws InterpretationException {
+        Command c = CommandFactory.getInstance("exit");
+        assertThat(c).isInstanceOf(ExitCommand.class);
+    }
+
+    @Test
+    public void shouldThrow_whenCommandNotFound() {
+        assertThatCode(() -> CommandFactory.getInstance("unknown command")).isInstanceOf(CommandNotFoundException.class);
+        assertThatCode(() -> CommandFactory.getInstance("abcde")).isInstanceOf(CommandNotFoundException.class);
+        assertThatCode(() -> CommandFactory.getInstance("test 123")).isInstanceOf(CommandNotFoundException.class);
     }
 
 }
